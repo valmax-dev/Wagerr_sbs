1b2d0fd240f5a71a998272f27788b19c70551dcd
iMac <iMac@iMac-de-iMac.local>
2020-09-13 16:00:56 +0200

    build url router

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,19 @@
+import { parseRequestUrl } from './utils.js'
 import HomeScreen from './screens/HomeScreen.js'
+import Error404Screen from './screens/Error404Screen.js'
+
+const routes = {
+  '/': HomeScreen,
+}
 const router = () => {
+  const request = parseRequestUrl()
+  const parseUrl =
+    (request.resource ? `/${request.resource}` : '/') +
+    (request.id ? '/:id' : '') +
+    (request.verb ? `/${request.verb}` : '')
+  const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen
   const main = document.getElementById('main-container')
-  main.innerHTML = HomeScreen.render()
+  main.innerHTML = screen.render()
 }
 window.addEventListener('load', router)
+window.addEventListener('hashchange', router)
