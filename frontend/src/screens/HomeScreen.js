195cb311ac26260bb6903a357833765a4db78215
iMac <iMac@iMac-de-iMac.local>
2020-09-19 02:00:14 +0200

    update HomeScreen UI

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,11 @@ import axios from 'axios'
 import { apiUrl } from '../config'
 
 const HomeScreen = {
+  after_render: () => {
+    document.getElementById('desktop').addEventListener('click', () => {
+      console.log('desktop')
+    })
+  },
   render: async () => {
     const response = await axios({
       url: `${apiUrl}/api/test`,
@@ -13,22 +18,36 @@ const HomeScreen = {
       return '<div>Error in getting data</div>'
     }
     // const test = response.data
+
     return `
     <div class='wrapper'>
       <div class='container'>
         <div class='left-menu'>
           <ul class='menu-list'>
             <li>
-              <div class='item'>
+              <div id='desktop' class='item'>
                 <img class='item-icon' src='../images/monitor.svg' alt='Desktop'>
                 <span>Desktop<span>
               </div>
             </li>
-            <li><div class='item'><img class='item-icon' src='../images/smartphone.svg' alt='Mobile'><span>Mobile<span></div></li>
-            <li><div class='item'><img class='item-icon' src='../images/code.svg' alt='Mobile'><span>Browser<span></div></li>
+            <li>
+              <div class='item'>
+                <img class='item-icon' src='../images/smartphone.svg' alt='Mobile'>
+                <span>Mobile<span>
+              </div>
+            </li>
+            <li>
+              <div class='item'>
+                <img class='item-icon' src='../images/code.svg' alt='Mobile'>
+                <span>Browser<span>
+              </div>
+            </li>
           </ul>
         </div>
-        <div class='right-content'><div>Select your betting device</div></div>
+        <div class='right-content'>
+          <img class='left-arrow' src='../images/left-curve-arrow.svg' alt='left-arrow'>
+          <p>Select your betting device</p>
+        </div>
       </div>
     </div>
  `
