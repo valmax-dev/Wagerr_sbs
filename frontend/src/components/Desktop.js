284db7634a42d45fa45eab98ba6ca1c1da431c36
iMac <iMac@iMac-de-iMac.local>
2020-09-20 23:38:46 +0200

    add desktop UI

--- frontend/src/components/Desktop.js
+++ frontend/src/components/Desktop.js
@@ -1,7 +1,23 @@
 const Desktop = {
   render: () => {
     return `
-    <h1>Dekstop</h1>`
+    <div class='desktop-container'>
+      <div class='wallets'>
+        <div class='wallet'>
+          <img class='wallet-icon' src='../images/electrum.png' alt='Desktop'>
+          <span>Electrum</span>
+        </div>
+        <div class='wallet'>
+          <img class='wallet-icon' src='../images/electron.png' alt='Desktop'>
+          <span>Electron</span>
+        </div>
+        <div class='wallet'>
+          <img class='wallet-icon' src='../images/qt.png' alt='Desktop'>
+          <span>Qt</span>
+        </div>
+      </div>
+    </div>
+    `
   },
 }
 
